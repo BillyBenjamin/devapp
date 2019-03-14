ac5b3a767f99ccf2328c136ddff88784389bec0d
Billy Benjamin <21701454t>
2019-03-14 10:58:52 +0100

    Style et tasks

--- js/main.js
+++ js/main.js
@@ -3,3 +3,12 @@ let tasks = new CompoTaskList({
     element: document.getElementById('tasks'),
     taskList: list
 });
+let task = new Task('test', null);
+let task2 = new Task('test2', null);
+let task3 = new Task('test3', null);
+
+list.add(task);
+list.add(task2);
+list.add(task3);
+
+tasks.render();
